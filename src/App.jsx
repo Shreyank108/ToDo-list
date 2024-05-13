a3f0d1a3a52cd614d21442e95ad4c791b569ffb2
shreyank108 <shreyankagrawal17@gmail.com>
2024-05-13 13:26:24 +0530

    Components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { nanoid } from "nanoid";
-import { useState } from "react";
+import { useState } from "react"; 
+import Header from "../src/Components/Header"
+import Middle from "../src/Components/Middle"
+import Footer from "../src/Components/Footer"
 
 const App = () => {
-    const [tasks, settasks] = useState(
+    const [tasks, setTasks] = useState(
         JSON.parse(localStorage.getItem("tasks")) || []
     );
 
-    const [title, settitle] = useState("");
+    const [title, setTitle] = useState("");
 
     const SubmitHandler = (e) => {
         e.preventDefault();
-        const newtodo = { id: nanoid(), title, completed: false };
-        console.log(newtodo);
+        const newTodo = { id: nanoid(), title, completed: false };
+        console.log(newTodo);
 
-        // const copytasks = [...tasks]
-        // copytasks.push(newtodo);
-        // settasks(copytasks)
-
-        settasks([...tasks, newtodo]);
-        settitle("");
-        localStorage.setItem("tasks", JSON.stringify([...tasks, newtodo]));
+        setTasks([...tasks, newTodo]);
+        setTitle("");
+        localStorage.setItem("tasks", JSON.stringify([...tasks, newTodo]));
     };
     console.log(tasks);
 
@@ -27,7 +26,7 @@ const App = () => {
         console.log(index);
         const copyTasks = [...tasks];
         copyTasks[index].completed = !copyTasks[index].completed;
-        settasks(copyTasks);
+        setTasks(copyTasks);
         localStorage.setItem("tasks", JSON.stringify(copyTasks));
     };
     
@@ -35,97 +34,27 @@ const App = () => {
         console.log(index);
         const copyTasks = [...tasks];
         copyTasks.splice(index,1);
-        settasks(copyTasks);
+        setTasks(copyTasks);
         localStorage.setItem("tasks", JSON.stringify(copyTasks));
     };
 
     return (
         <div className="overflow-x-hidden border-t-2 w-screen min-h-[100vh] bg-zinc-800 flex  items-center flex-col">
             {/*  */}
-            <div className="mt-[7%] w-[35%] h-[30vh] border rounded-3xl flex justify-around items-center">
-                <div className="text-yellow-100">
-                    <h1 className="text-3xl font-bold">LETS TODO</h1>
-                    <p>Keeps doing things</p>
-                </div>
-                <div className="text-3xl font-extrabold flex justify-center items-center w-[10vmax] h-[10vmax] rounded-full bg-orange-600">
-                    {tasks.filter((t) => t.completed === true).length}/
-                    {tasks.length}
-                </div>
-            </div>
+            <Header tasks={tasks} />
             {/*  */}
-            <form
-                onSubmit={SubmitHandler}
-                className="w-[35%] flex justify-between px-5 my-[2%]"
-            >
-                <input
-                    onChange={(e) => settitle(e.target.value)}
-                    value={title}
-                    placeholder="write your next task..."
-                    className="px-5 py-2 text-yellow-100 outline-none w-[80%] rounded-xl bg-zinc-700 "
-                    type="text"
-                />
-                <button className="outline-none text-4xl font-extrabold flex justify-center items-center w-[5vmax] h-[5vmax] rounded-full bg-orange-600">
-                    <i className="ri-add-fill"></i>
-                </button> 
-
-            </form>
+            <Middle 
+                SubmitHandler={SubmitHandler}
+                settitle={setTitle}
+                title={title}
+            />
             {/*  */}
-
-            <ul className="list-none w-[35%] ">
-                {tasks.length > 0 ? (
-                    tasks.map((task, index) => {
-                        return (
-                            <li
-                                key={task.id}
-                                className="mb-5 flex justify-between items-center border rounded-xl p-5"
-                            >
-                                <div className="flex items-center">
-                                    <div
-                                        onClick={() => CompleteHandler(index)}
-                                        className={`${
-                                            task.completed
-                                                ? "bg-green-600"
-                                                : "border"
-                                        } mr-4 rounded-full w-[30px] h-[30px]  border-orange-600`}
-                                    ></div> 
-                                    <h1
-                                        className={`${
-                                            task.completed ? "line-through" : ""
-                                        } text-2xl font-extrabold text-yellow-100`}
-                                    >
-                                        {task.title}
-                                    </h1>
-                                </div>
-                                <div className="flex gap-3 text-2xl text-yellow-100">
-                                    <i className="ri-file-edit-line"></i>
-                                    <i onClick={()=>deleteHandler(index)} className="ri-delete-bin-3-line"></i>
-                                </div>
-                            </li>
-                        );
-                    })
-                ) : (
-                    <h1 className="mt-10 w-full text-center text-orange-600 text-3xl">
-                        No Pending Tasks
-                    </h1>
-                )}
-
-                {/* <li className="mb-5 flex justify-between items-center border rounded-xl p-5">
-                    <div className="flex items-center">
-                        <div
-                            className={`bg-green-600 mr-4 rounded-full w-[30px] h-[30px]  border-orange-600`}
-                        ></div>
-                        <h1
-                            className={`line-through text-2xl font-extrabold text-yellow-100`}
-                        >
-                            Task 01
-                        </h1>
-                    </div>
-                    <div className="flex gap-3 text-2xl text-yellow-100">
-                        <i className="ri-file-edit-line"></i>
-                        <i className="ri-delete-bin-3-line"></i>
-                    </div>
-                </li> */}
-            </ul>
+            <Footer 
+                CompleteHandler={CompleteHandler} 
+                deleteHandler={deleteHandler} 
+                tasks={tasks}
+            />
+           
         </div>
     );
 };
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Footer = ({CompleteHandler , deleteHandler, tasks}) => {
+  return (
+    <>
+        <ul className="list-none w-[35%] ">
+                {tasks.length > 0 ? (
+                    tasks.map((task, index) => {
+                        return (
+                            <li
+                                key={task.id}
+                                className="mb-5 flex justify-between items-center border rounded-xl p-5"
+                            >
+                                <div className="flex items-center">
+                                    <div
+                                        onClick={() => CompleteHandler(index)}
+                                        className={`${
+                                            task.completed
+                                                ? "bg-green-600"
+                                                : "border"
+                                        } mr-4 rounded-full w-[30px] h-[30px]  border-orange-600`}
+                                    ></div> 
+                                    <h1
+                                        className={`${
+                                            task.completed ? "line-through" : ""
+                                        } text-2xl font-extrabold text-yellow-100`}
+                                    >
+                                        {task.title}
+                                    </h1>
+                                </div>
+                                <div className="flex gap-3 text-2xl text-yellow-100">
+                                    <i className="ri-file-edit-line"></i>
+                                    <i onClick={()=>deleteHandler(index)} className="ri-delete-bin-3-line"></i>
+                                </div>
+                            </li>
+                        );
+                    })
+                ) : (
+                    <h1 className="mt-10 w-full text-center text-orange-600 text-3xl">
+                        No Pending Tasks
+                    </h1>
+                )}
+            </ul>
+    </>
+  )
+}
+
+export default Footer
